197343988d033de806ad4287b768f303900e1917
Vladimir Simin <vladimir.simin@gmail.com>
2021-11-18 21:01:30 +0100

    task 2 - posts listing

--- src/pages/AppPosts.js
+++ src/pages/AppPosts.js
@@ -1,7 +1,26 @@
-
+import React, { useState, useEffect } from 'react';
+import PostService from "../services/PostService";
 
 export default function AppPosts() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        async function retrievePosts() {
+            const data = await PostService.getAll();
+            setPosts(data);
+        };
+        retrievePosts();
+    }, []);
+
     return (
-        <h2>Posts</h2>
+        <div>
+            <h1>Posts</h1>
+            {posts.map((post) => (
+                <div style={{ display: 'flex', flexDirection: 'column', }} key={post.id}>
+                    <p>Title: {post.title}</p>
+                    <p>Text:{post.text}</p>
+                </div>
+            ))}
+        </div>
     );
 }
